c0a45dfe237f2aa8ec6147063b04161da8ac2a80
AMagicRake <niel.gow@live.co.uk>
2024-08-06 18:53:02 +0100

    Added additional cmd line handling

--- primeapp/main.go
+++ primeapp/main.go
@@ -1,15 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 
-	n := 5
+	// Print a welcome message
+	intro()
+	// create a channel to indicate when a user wants to quit
+	doneChan := make(chan bool)
+	// start a go routine to read user input and run program
+	go readUserInput(doneChan)
+	//block until the doneChan gets a value
+	<-doneChan
+	// close the channel
+	close(doneChan)
+	//say goodbye
+	fmt.Println("Goodbye...")
+}
 
-	_, msg := isPrime(n)
+func intro() {
+	fmt.Println("Is it prime?")
+	fmt.Println("------------")
+	fmt.Println("Enter a whole number, and we'll tell you if it is a prime number or not. Enter q to quit.")
+	prompt()
+}
 
-	fmt.Println(msg)
+func prompt() {
+	fmt.Print("-> ")
+}
 
+func readUserInput(doneChan chan bool) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		res, done := checkNumbers(scanner)
+
+		if done {
+			doneChan <- true
+			return
+		}
+
+		fmt.Println(res)
+		prompt()
+	}
+}
+
+func checkNumbers(scanner *bufio.Scanner) (string, bool) {
+	//read user input
+	scanner.Scan()
+
+	//check to see if user wants to quit
+	if strings.EqualFold(scanner.Text(), "q") {
+		return "", true
+	}
+
+	numToCheck, err := strconv.Atoi(scanner.Text())
+
+	if err != nil {
+		return "Please enter a whole number", false
+	}
+
+	_, msg := isPrime(numToCheck)
+	return msg, false
 }
 
 func isPrime(n int) (bool, string) {
