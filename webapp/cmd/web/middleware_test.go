06be934e3749db1abe32a7fe4b5e32fcd709a22e
AMagicRake <niel.gow@live.co.uk>
2024-08-09 10:57:38 +0100

    made middleware test

---
+++ webapp/cmd/web/middleware_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var contextTests = []struct {
+	headerName  string
+	headerValue string
+	addr        string
+	emptyAddr   bool
+}{
+	{headerName: "", headerValue: "", addr: "", emptyAddr: false},
+	{headerName: "", headerValue: "", addr: "", emptyAddr: true},
+	{headerName: "X-Forwarded-For", headerValue: "192.2.2.1", addr: "", emptyAddr: false},
+	{headerName: "", headerValue: "", addr: "hello:world", emptyAddr: false},
+}
+
+func Test_application_addIPToContext(t *testing.T) {
+
+	app := &application{}
+
+	// createa dummy handler that we'll use to checkthe context
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//make sure that the value exists in the context
+		val := r.Context().Value(contextUserKey)
+		if val == nil {
+			t.Error(contextUserKey, "not present")
+		}
+
+		// make sure we got a string back
+
+		ip, ok := val.(string)
+		if !ok {
+			t.Error("not string")
+		}
+		t.Log(ip)
+
+	})
+
+	for _, e := range contextTests {
+		handlerToTest := app.addIPToContext(nextHandler)
+
+		req := httptest.NewRequest("GET", "http://testing", nil)
+
+		if e.emptyAddr {
+			req.RemoteAddr = ""
+		}
+
+		if len(e.headerName) > 0 {
+			req.Header.Add(e.headerName, e.headerValue)
+		}
+
+		if len(e.addr) > 0 {
+			req.RemoteAddr = e.addr
+		}
+
+		handlerToTest.ServeHTTP(httptest.NewRecorder(), req)
+	}
+
+}
