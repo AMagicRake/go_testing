dc3970c5e4cb1c1533d5b41582a4c96e33f2147c
AMagicRake <niel.gow@live.co.uk>
2024-08-09 11:12:35 +0100

    create login form stub for generating tests

--- webapp/cmd/web/routes.go
+++ webapp/cmd/web/routes.go
@@ -16,6 +16,7 @@ func (app *application) routes() http.Handler {
 
 	// register routes
 	mux.Get("/", app.Home)
+	mux.Post("/login", app.login)
 	// static assets
 	fileServer := http.FileServer(http.Dir("./static"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
