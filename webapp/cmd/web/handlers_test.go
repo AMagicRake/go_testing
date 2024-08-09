d064ef6c9e7d28cc06bb3f6482501b70c2cdfbd0
AMagicRake <niel.gow@live.co.uk>
2024-08-09 10:20:40 +0100

    added test for handlers and routes

---
+++ webapp/cmd/web/handlers_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var pageTests = []struct {
+	name               string
+	url                string
+	expectedStatusCode int
+}{
+	{name: "home", url: "/", expectedStatusCode: http.StatusOK},
+	{name: "404", url: "/forced404", expectedStatusCode: http.StatusNotFound},
+}
+
+func Test_application_home(t *testing.T) {
+
+	app := &application{}
+	routes := app.routes()
+
+	//spawn test http server
+	ts := httptest.NewTLSServer(routes)
+	defer ts.Close()
+
+	//range through the test data
+	pathToTemplates = "./../../templates"
+	for _, e := range pageTests {
+		resp, err := ts.Client().Get(ts.URL + e.url)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if resp.StatusCode != e.expectedStatusCode {
+			t.Errorf("%s: expected status %d, but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+		}
+	}
+
+}
