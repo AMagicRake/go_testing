ef0d793c2653f1d49842ad3e6fe329a3252d0f9e
AMagicRake <niel.gow@live.co.uk>
2024-08-11 12:45:03 +0100

    creating db connection functionality

--- webapp/cmd/web/main.go
+++ webapp/cmd/web/main.go
@@ -1,26 +1,39 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"webapp/pkg/db"
 
 	"github.com/alexedwards/scs/v2"
 )
 
 type application struct {
 	Session *scs.SessionManager
+	DSN     string
+	DB      db.PostgresConn
 }
 
 func main() {
 	// set up an app config
 	app := application{}
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5", "Postgres Connection")
+	flag.Parse()
+
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.DB = db.PostgresConn{DB: conn}
 	app.Session = getSession()
 
 	// print out a message
 	log.Println("Starting server on port 8080...")
 
 	// start the server
-	err := http.ListenAndServe(":8080", app.routes())
+	err = http.ListenAndServe(":8080", app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
