e333e18bff4f6b4de5258b5803fad835fa809911
AMagicRake <niel.gow@live.co.uk>
2024-08-12 16:49:52 +0100

    created profile pic upload routes

--- webapp/cmd/web/handlers.go
+++ webapp/cmd/web/handlers.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"path"
+	"path/filepath"
 	"time"
 	"webapp/pkg/data"
 )
@@ -46,6 +50,10 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, t string,
 	td.IP = app.ipFromContext(r.Context())
 	td.Error = app.Session.PopString(r.Context(), "error")
 	td.Flash = app.Session.PopString(r.Context(), "flash")
+
+	if app.Session.Exists(r.Context(), "user") {
+		td.User = app.Session.Get(r.Context(), "user").(data.User)
+	}
 
 	// execute template, passing it data if any
 	err = parsedTemplate.Execute(w, td)
@@ -113,4 +121,91 @@ func (app *application) authenticate(r *http.Request, user *data.User, password 
 	app.Session.Put(r.Context(), "user", user)
 
 	return true
+}
+
+func (app *application) UploadProfilePicture(w http.ResponseWriter, r *http.Request) {
+	// call a function that extracts the file form and upload
+	uploads, err := app.UploadFiles(r, "./static/img")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// get the user from the session
+	user := app.Session.Get(r.Context(), "user").(data.User)
+
+	// create a variable of type data.UserImage
+	i := data.UserImage{
+		UserID:   user.ID,
+		FileName: uploads[0].OriginalFileName,
+	}
+
+	// insert UserImage into user_images
+	_, err = app.DB.InsertUserImage(i)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	// refresh session variable "user"
+	updatedUser, err := app.DB.GetUser(user.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	app.Session.Put(r.Context(), "user", updatedUser)
+
+	// redirect back to profile page
+	app.Session.Put(r.Context(), "flash", "Upload Succesful")
+	http.Redirect(w, r, "/user/profile", http.StatusSeeOther)
+}
+
+type UploadedFile struct {
+	OriginalFileName string
+	FileSize         int64
+}
+
+func (app *application) UploadFiles(r *http.Request, uploadDir string) ([]*UploadedFile, error) {
+	uploadedFiles := []*UploadedFile{}
+
+	err := r.ParseMultipartForm(int64(1024 * 1024 * 5)) //5MB
+	if err != nil {
+		return nil, fmt.Errorf("the uploaded file is too big and must be less than %d bytes", 1024*1024*5)
+	}
+
+	for _, fHeaders := range r.MultipartForm.File {
+		for _, hdr := range fHeaders {
+			uploadedFiles, err = func(uploadedFiles []*UploadedFile) ([]*UploadedFile, error) {
+				uploadedFile := UploadedFile{}
+				infile, err := hdr.Open()
+				if err != nil {
+					return nil, err
+				}
+				defer infile.Close()
+
+				uploadedFile.OriginalFileName = hdr.Filename
+
+				var outfile *os.File
+				defer outfile.Close()
+
+				if outfile, err = os.Create(filepath.Join(uploadDir, uploadedFile.OriginalFileName)); err != nil {
+					return nil, err
+				} else {
+					fileSize, err := io.Copy(outfile, infile)
+					if err != nil {
+						return nil, err
+					}
+					uploadedFile.FileSize = fileSize
+				}
+
+				uploadedFiles = append(uploadedFiles, &uploadedFile)
+
+				return uploadedFiles, nil
+			}(uploadedFiles)
+
+			if err != nil {
+				return uploadedFiles, err
+			}
+		}
+	}
+
+	return uploadedFiles, nil
 }
